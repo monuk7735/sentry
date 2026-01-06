b25b86833bffb834ff5ae91f6f779eb0589301b0
Monu Kumar <monuk7735@gmail.com>
2026-01-06 23:35:18 +0530

    Initial Commit

---
+++ Sentry/SentryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentryApp.swift
+//  Sentry
+//
+//  Created by Monu Kumar on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SentryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
